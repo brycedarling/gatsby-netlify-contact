687881ce0febc66df52dc597ff48933a8dffe599
Bryce Darling <bryce@runesoftware.com>
2020-07-27 23:04:59 -0400

    Add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,39 @@
+
+
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Contact" />
+    <form
+      name="contact"
+      method="POST"
+      action="/contacted"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+    >
+      <label>
+        Name
+        <input type="text" name="name" required />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" required />
+      </label>
+      <label>
+        Phone
+        <input type="tel" name="phone" />
+      </label>
+      <label>
+        Message
+        <textarea name="message" required />
+      </label>
+      <button type="submit">Send</button>
+    </form>
+  </Layout>
+)
+
+export default ContactPage
