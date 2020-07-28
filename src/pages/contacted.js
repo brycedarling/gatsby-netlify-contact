8829684e267050051085f94ae5befec409ea8fa3
Bryce Darling <bryce@runesoftware.com>
2020-07-27 23:05:11 -0400

    Add contacted page

---
+++ src/pages/contacted.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactedPage = () => (
+  <Layout>
+    <SEO title="Thank you for contacting us!" />
+    <h1>Thanks for contacting us!</h1>
+    <p>We will be in touch with you shortly.</p>
+  </Layout>
+)
+
+export default ContactedPage
